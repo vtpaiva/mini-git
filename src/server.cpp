2ec693e80eff3d570d6d1dc91b878da78c1c7db7
vtpaiva <vitor_brito@usp.br>
2024-09-19 00:27:30 -0300

    Criação de pasta para armazenamento dos repositórios e servidor só aborta no momento em que todas as threads terminam

--- src/server.cpp
+++ src/server.cpp
@@ -1,50 +1,41 @@
 #include "../header/header.hpp"
 
-void accept_client(SOCKET server_socket, SOCKET client_socket, std::string buffer) {
+const std::string REPOS_DIR = "repos/";
+constexpr const char* LOGIN_MESSAGE = "Login completed!";
+
+int accept_client(SOCKET server_socket, SOCKET client_socket, std::string buffer) {
     client *new_client = new client(client_socket);
 
-    new_client -> copy_data_to_buffer(&buffer[0]);
+    send(client_socket, LOGIN_MESSAGE, BUFFER_SIZE, 0);
+    std::cout << "Login: \"" << new_client -> name << "\"!" << std::endl;
 
-    send(client_socket, &buffer[0], BUFFER_SIZE, 0);
-    std::cout << "Login sent to " << new_client -> name << std::endl;
-
-    std::filesystem::create_directory(new_client->name);
-
-    buffer = std::string(FILE_NAME_SIZE, '\0');
+    std::filesystem::create_directory(REPOS_DIR + new_client->name);
 
     int received_bytes = recv(client_socket, &buffer[0], BUFFER_SIZE, 0) - 1;
     buffer.resize(received_bytes);
 
-    std::ofstream client_file(new_client->name + "/" + buffer);
-
+    std::ofstream client_file(REPOS_DIR + new_client->name + "/" + buffer);
     client_file.close();
 
-    exit(EXIT_SUCCESS);
+    std::cout << "Logout: \"" << new_client -> name << "\"!" << std::endl;
+
+    return EXIT_SUCCESS;
 }
 
-void handle_connect(SOCKET server_socket, SOCKET client_socket, std::string buffer) {
-    std::vector<std::thread> threads;
-
+void handle_connect(SOCKET server_socket, SOCKET client_socket, std::string buffer, std::vector<std::thread> &threads) {
     while(client_socket = accept(server_socket, nullptr, nullptr)) {
         threads.push_back(std::thread(accept_client, server_socket, client_socket, buffer));
     }
-
-    for(auto& t: threads) {
-        t.join();
-    }
 }
 
-void handle_input() {
+int handle_input() {
     std::string input(COMMAND_SIZE, '\0');
 
     while(true) {
         std::getline(std::cin, input);
 
-        if (input == "exit") {
-            std::cout << "Shutting down the server..." << std::endl;
-
-            exit(EXIT_SUCCESS);            
-        }
+        if (input == "exit")
+            return EXIT_SUCCESS;            
     }
 }
 
@@ -53,14 +44,14 @@ int main(int argc, char **argv) {
     int n_bind, n_listen;
     std::string buffer(1024, '\0');
 
+    std::vector<std::thread> threads;
+
     exit_if_error(server_socket = socket(AF_INET, SOCK_STREAM, 0), 
                   SOCKET_CREATION_ERROR);
 
     struct sockaddr_in serv_addr = {sin_family: AF_INET, 
                                     sin_port: htons(PORT),
                                     sin_addr: {s_addr: INADDR_ANY}};
-
-    socklen_t addr_len = sizeof(serv_addr);
     memset(&serv_addr.sin_zero, '0', 8);
 
     exit_if_error(n_bind = bind(server_socket, (sockaddr*) &serv_addr, sizeof(sockaddr_in)), 
@@ -71,13 +62,18 @@ int main(int argc, char **argv) {
 
     std::cout << "Waiting for connections..." << std::endl;
 
-    std::thread connect_thread(handle_connect, server_socket, client_socket, buffer);
-
     std::thread input_thread(handle_input);
+    std::thread connect_thread(handle_connect, server_socket, client_socket, buffer, std::ref(threads));
+
     input_thread.join();
+
+    for(auto& th: threads) 
+        th.join();
+
+    std::cout << "Shutting down the server..." << std::endl;
 
     close(client_socket);
     close(server_socket);
 
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
